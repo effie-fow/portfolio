3db5c9d5cb88a0d6a7435343365f3582232d1738
Effie Fowler <effiejofowler@gmail.com>
2024-10-09 15:46:15 +0100

    add base structure for home/entry page and styling for rotating background

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import "./App.scss";
+import backroundImage from "./assets/graphics/background-blobs.png";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./components/Home/Home";
 
 function App() {
   return (
-    <>
-      <main className="main">
-        <div>TEST</div>
-      </main>
-    </>
+    <main className="main">
+      <img
+        src={backroundImage}
+        alt="Background image of floating, pastel-coloured blobs"
+        className="main__background"
+      />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </main>
   );
 }
 
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,10 @@
+import "./Home.scss";
+
+export const Home = () => {
+  return (
+    <main className="home">
+      <h1 className="home__heading">Effie Fowler</h1>
+      <span className="home__subheading">I build web apps, and love it.</span>
+    </main>
+  );
+};

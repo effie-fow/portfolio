71d078117600054af4706d92ee8536d249dd0f73
Effie Fowler <effiejofowler@gmail.com>
2024-12-24 10:58:07 +0000

    Remove contact message

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -9,12 +9,6 @@ export const Contact = () => {
       <div className="contact__flex-container">
         <h2 className="contact__heading">Effie Fowler</h2>
         <span className="contact__subheading">Software Engineer</span>
-        <p className="contact__details">
-          Whether you are a fellow developer looking to connect, or a recruiter
-          whose interest I have piqued, I would be happy to hear from you.
-          Please feel free to reach out to me on LinkedIn or GitHub using the
-          links below.
-        </p>
         <span className="contact__thank-you">Thanks for stopping by!</span>
         <div className="contact__buttons-container">
           <ButtonCircle
